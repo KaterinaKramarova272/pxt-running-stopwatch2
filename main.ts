cd845bc5f7142905958dd9088f5417c24baede1d
Pho3Nix <katerina.krama.024@pslib.cz>
2025-02-24 10:08:47 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,46 @@
 radio.setGroup(69)
 let ready = true
 
-input.onButtonPressed(Button.A, function () {
+reset ()
+
+radio.setTransmitPower (7)
+
+function reset() {
     Sensors.SetLightLevel()
+    basic.showNumber(1)
+    ready = true
 }
-)
+
+input.onButtonPressed (Button.A, function (){
+    Sensors.SetLightLevel ()
+})
 
 Sensors.OnLightDrop(function () {
-    if (ready === true)
-        radio.sendNumber(0)
+    if (ready === true) {
+            radio.sendNumber(0)
+            music.playTone (320, 250)
+            basic.showNumber(2)
         ready = false
+    }
 })
 
 radio.onReceivedValue(function(name: string, value: number) {
-    if (name === "cas") 
+    if (name == "elapsedT") {
+        music.playTone(320, 250)
         basic.showNumber (value)
+    }
+input.onButtonPressed(Button.B, function () {
+        basic.showNumber(value)
+    })
+})
+
+radio.onReceivedString(function(receivedString: string) {
+    if (receivedString == "resetuj") {
+        reset ()
+    }
+})
+
+input.onLogoEvent (TouchButtonEvent.Pressed, function (){
+    radio.sendString("resetuj2")
+    reset ()
 })
